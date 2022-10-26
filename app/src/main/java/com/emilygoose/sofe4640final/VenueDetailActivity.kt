f4760d3604714858a4e6e3a8f197987bab306ec8
EmilyGoose <EmilyGoose@users.noreply.github.com>
2022-10-26 14:27:40 -0400

    Boilerplate VenueDetailActivity

---
+++ app/src/main/java/com/emilygoose/sofe4640final/VenueDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.emilygoose.sofe4640final
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VenueDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_venue_detail)
+
+
+    }
+}
