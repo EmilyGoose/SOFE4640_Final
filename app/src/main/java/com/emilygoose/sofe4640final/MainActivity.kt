0e3e96952328f62d85f3adad5dc2b1d6a1c5b441
EmilyGoose <EmilyGoose@users.noreply.github.com>
2022-10-26 09:50:27 -0400

    App gets user location

--- app/src/main/java/com/emilygoose/sofe4640final/MainActivity.kt
+++ app/src/main/java/com/emilygoose/sofe4640final/MainActivity.kt
@@ -1,8 +1,16 @@
 package com.emilygoose.sofe4640final
 
+import android.Manifest
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.emilygoose.sofe4640final.util.Ticketmaster
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -12,12 +20,46 @@ class MainActivity : AppCompatActivity() {
     // Firebase auth
     private lateinit var auth: FirebaseAuth
 
+    // Location services client
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    // Ticketmaster API client helper
+    private val ticketmaster = Ticketmaster()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // Initialize Firebase auth
         auth = Firebase.auth
+
+        // Initiate the fusedLocationClient
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        // Check that we have location permissions, if not then ask the user
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            Log.d("Location", "No location perms, ask the user")
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
+                0
+            )
+        }
+
+        // Get the user's last known location and populate the nearby venues list
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location: Location? ->
+                Log.d("Location", "Got user's location, calling Ticketmaster API")
+                // Get nearby venues - In rare cases location is null so send user to null island
+                ticketmaster.getNearbyVenues(location ?: Location(null))
+            }
     }
 
     override fun onStart() {
--- app/src/main/java/com/emilygoose/sofe4640final/util/Ticketmaster.kt
+++ app/src/main/java/com/emilygoose/sofe4640final/util/Ticketmaster.kt
@@ -1,7 +1,12 @@
 package com.emilygoose.sofe4640final.util
 
+import android.location.Location
+import android.util.Log
 import com.emilygoose.sofe4640final.BuildConfig
+import com.firebase.geofire.GeoFireUtils
+import com.firebase.geofire.GeoLocation
 import okhttp3.OkHttpClient
+import okhttp3.Request
 
 class Ticketmaster {
     // OkHttp client for calling request
@@ -12,5 +17,20 @@ class Ticketmaster {
 
     // Api key (big secret, get from apikey.properties)
     private var apiKey: String = BuildConfig.TICKETMASTER_API_KEY
+
+    // Get nearby venues - TODO return something lol
+    fun getNearbyVenues(location: Location) {
+        // Get GeoHash for the location and truncate to 9 chars
+        // Ticketmaster API wants max 9
+        val geoHash =
+            GeoFireUtils.getGeoHashForLocation(GeoLocation(location.latitude, location.longitude))
+                .substring(0, 9)
+
+        Log.d("VenueSearch", "User GeoHash: $geoHash")
+
+
+
+    }
+
 
 }
