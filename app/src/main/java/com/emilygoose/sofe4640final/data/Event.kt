483ffd0c57a6a168170ef90bcc05393b06a8d6fa
EmilyGoose <EmilyGoose@users.noreply.github.com>
2022-11-19 15:39:53 -0500

    Added dates to Event data class

--- app/src/main/java/com/emilygoose/sofe4640final/data/Event.kt
+++ app/src/main/java/com/emilygoose/sofe4640final/data/Event.kt
@@ -6,6 +6,19 @@ import kotlinx.serialization.Serializable
 data class Event(
     val type: String,
     val name: String,
+    val dates: Dates,
     val description: String = "",
     val images: List<Image> = ArrayList()
-)
+) {
+    // Ugly nested serializable because of the weird format from the API
+    @Serializable
+    data class Dates(
+        val start: StartDate
+    ) {
+        @Serializable
+        data class StartDate(
+            val localDate: String,
+            val dateTime: String
+        )
+    }
+}
